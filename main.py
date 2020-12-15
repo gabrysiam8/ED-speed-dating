b293250aec53a185a975421ea170b79a429cd448
gabrysiam8 <gabrysiamiedlar@o2.pl>
2020-12-15 21:06:21 +0100

    Add PCA

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import numpy as np
 import pandas as pd
-from IPython.display import display
+from sklearn.impute import SimpleImputer
+from sklearn.decomposition import PCA
 
 
 def merge_person_partner_data(f_df, m_df):
@@ -14,27 +16,46 @@ def merge_person_partner_data(f_df, m_df):
     return f_df.copy()
 
 
+def detect_outliers(original_df, columns):
+    # outliers detection (for now only for interests correlation)
+    for col in columns:
+        mean = original_df[col].mean()
+        std = original_df[col].std()
+        outliers_df = pd.DataFrame()
+        outliers_df['is_outlier'] = abs(original_df[col] - mean) > 3 * std
+        # print outliers
+        print('Outlier values for ' + col + ': ')
+        print(original_df[outliers_df.any(axis=1)])
+        outlier_sum = outliers_df.is_outlier.sum()
+        print('Number of outliers for ' + col + ': ' + str(outlier_sum))
+        # remove outliers from original dataframe
+        original_df = original_df[~outliers_df.any(axis=1)]
+
+    return original_df
+
+
+def replace_missing_values(original_df):
+    imp_mean = SimpleImputer(missing_values=np.nan, strategy='mean')
+    imp_mean = imp_mean.fit(original_df.values)
+    return pd.DataFrame(data=imp_mean.transform(original_df.values), columns=column_names)
+
+
+def two_components_pca(original_df):
+    pca = PCA(n_components=2)
+    principal_components = pca.fit_transform(original_df.values)
+    pca_df = pd.DataFrame(data=principal_components, columns=['PC 1', 'PC 2'])
+    return pca_df
+
+
 if __name__ == '__main__':
     df = pd.read_csv('Speed Dating Data.csv', encoding="ISO-8859-1")
 
     column_names = ['iid', 'gender', 'race', 'age', 'field_cd', 'career_c', 'int_corr', 'attr1_1', 'sinc1_1',
-                    'intel1_1', 'fun1_1', 'amb1_1', 'shar1_1', 'attr7_2', 'sinc7_2', 'intel7_2', 'fun7_2', 'amb7_2',
-                    'shar7_2', 'match', 'pid']
+                    'intel1_1', 'fun1_1', 'amb1_1', 'shar1_1', 'match', 'pid']
     df = df[column_names]
     print(df)
 
-    # outliers detection (for now only for interests correlation)
-    for col in ['int_corr']:
-        mean = df[col].mean()
-        std = df[col].std()
-        print(mean, std)
-        outliers_df = pd.DataFrame()
-        outliers_df['is_outlier'] = abs(df[col] - mean) > 3 * std
-        with_outliers_df = pd.merge(df, outliers_df, left_index=True, right_index=True, how='left')
-        display(with_outliers_df[with_outliers_df['is_outlier']])
-        outlier_sum = with_outliers_df.is_outlier.sum()
-        print('Number of outliers for ' + col + ': ' + str(outlier_sum))
-        df = with_outliers_df[with_outliers_df['is_outlier'] == False].drop(['is_outlier'], axis=1)
+    df = detect_outliers(df, ['int_corr'])
 
     print(df)
 
@@ -53,8 +74,15 @@ if __name__ == '__main__':
     race_stat = race_stat.append(pd.DataFrame([[0, notclassified, 'notclassified']], columns=['race', 'count', 'value']))
     print(race_stat)
 
-    female_df = df[df['gender'] == 0].copy()
-    male_df = df[df['gender'] == 1].copy()
-    merged_df = merge_person_partner_data(female_df, male_df)
+    # replace missing values with mean
+    df = replace_missing_values(df)
 
-    print(merged_df)
+    # Principal Component Analysis
+    principal_df = two_components_pca(df)
+    print(principal_df)
+
+    # female_df = df[df['gender'] == 0].copy()
+    # male_df = df[df['gender'] == 1].copy()
+    # merged_df = merge_person_partner_data(female_df, male_df)
+    #
+    # print(merged_df)
