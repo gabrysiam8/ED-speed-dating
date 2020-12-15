351a422d3215e00e97314722de44c1e35180e7a7
gabrysiam8 <gabrysiamiedlar@o2.pl>
2020-12-15 19:27:42 +0100

    Add function to merge person and partner data

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 import pandas as pd
 from IPython.display import display
 
+
+def merge_person_partner_data(f_df, m_df):
+    columns_to_merge = ['gender', 'race', 'age', 'field_cd', 'career_c', 'int_corr', 'attr1_1', 'sinc1_1', 'intel1_1',
+                        'fun1_1', 'amb1_1', 'shar1_1', 'attr7_2', 'sinc7_2', 'intel7_2', 'fun7_2', 'amb7_2', 'shar7_2']
+
+    for index, row in f_df.iterrows():
+        partner = m_df[(m_df['iid'] == row['pid']) & (m_df['pid'] == row['iid'])].iloc[0]
+        for column in columns_to_merge:
+            f_df.loc[index, 'p_' + column] = partner[column]
+
+    return f_df.copy()
+
+
 if __name__ == '__main__':
     df = pd.read_csv('Speed Dating Data.csv', encoding="ISO-8859-1")
 
-    column_names = ['iid', 'pid', 'gender', 'race', 'age', 'field_cd', 'career_c', 'int_corr', 'attr1_1', 'sinc1_1',
+    column_names = ['iid', 'gender', 'race', 'age', 'field_cd', 'career_c', 'int_corr', 'attr1_1', 'sinc1_1',
                     'intel1_1', 'fun1_1', 'amb1_1', 'shar1_1', 'attr7_2', 'sinc7_2', 'intel7_2', 'fun7_2', 'amb7_2',
-                    'shar7_2', 'match']
+                    'shar7_2', 'match', 'pid']
     df = df[column_names]
     print(df)
 
@@ -28,14 +41,20 @@ if __name__ == '__main__':
     df_without_duplicates = df.drop_duplicates(subset=['iid'])
     race_stat = df_without_duplicates.groupby(['race']).size().rename("count").to_frame().reset_index()
 
-    dict = {1: 'black', 2:'white', 3:'latino', 4: 'asian', 5: 'native', 6: 'other'}
+    dict = {1: 'black', 2: 'white', 3: 'latino', 4: 'asian', 5: 'native', 6: 'other'}
 
     race_stat['value'] = race_stat['race'].map(dict)
     notclassified = df_without_duplicates.shape[0] - race_stat['count'].sum()
-    for i in range(1,6):
+    for i in range(1, 6):
         if not (i in race_stat.race):
             race_stat = race_stat.append(
                 pd.DataFrame([[i, 0, dict[i]]], columns=['race', 'count', 'value']))
 
     race_stat = race_stat.append(pd.DataFrame([[0, notclassified, 'notclassified']], columns=['race', 'count', 'value']))
     print(race_stat)
+
+    female_df = df[df['gender'] == 0].copy()
+    male_df = df[df['gender'] == 1].copy()
+    merged_df = merge_person_partner_data(female_df, male_df)
+
+    print(merged_df)
