833de7875bef32cc39ac6538bf56c4067b32a870
gabrysiam8 <gabrysiamiedlar@o2.pl>
2020-12-21 22:00:54 +0100

    Clean up

--- main.py
+++ main.py
@@ -11,9 +11,8 @@ from scipy.cluster.hierarchy import dendrogram, linkage
 from sklearn.model_selection import train_test_split
 import seaborn as sns
 from numpy import loadtxt
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn import metrics
 from sklearn.metrics import classification_report, confusion_matrix
+import pylab as pl
 
 
 def merge_person_partner_data(f_df, m_df):
@@ -56,6 +55,7 @@ def replace_missing_values(original_df):
 def run_pca(n_components, original_df):
     pca = PCA(n_components=n_components)
     principal_components = pca.fit_transform(original_df.values)
+    print('Eigenvectors for the projection space:')
     print(pca.components_)
     pca_df = pd.DataFrame(data=principal_components, index=original_df.index)
     pca_df.rename({i: "PC{}".format(i) for i in range(n_components)}, axis=1, inplace=True)
@@ -115,13 +115,9 @@ def draw_cluster_barplot(original_df, model):
 if __name__ == '__main__':
     df = pd.read_csv('Speed Dating Data.csv', encoding="ISO-8859-1")
 
-    column_names = ['iid', 'pid', 'gender', 'race', 'age', 'field_cd', 'career_c', 'int_corr', 'attr1_1', 'sinc1_1',
-                    'intel1_1', 'fun1_1', 'amb1_1', 'shar1_1', 'match', 'age_o', 'samerace']
+    column_names = ['iid', 'pid', 'gender', 'race', 'age', 'field_cd', 'int_corr', 'attr1_1', 'sinc1_1',
+                    'intel1_1', 'fun1_1', 'amb1_1', 'shar1_1', 'match', 'age_o']
     df = df[column_names]
-
-    for col in column_names[2:]:
-        sns.displot(df, x=col)
-        plt.show()
 
     df_without_duplicates = df.drop_duplicates(subset=['iid'])
     race_stat = df_without_duplicates.groupby(['race']).size().rename("count").to_frame().reset_index()
@@ -137,11 +133,6 @@ if __name__ == '__main__':
                 pd.DataFrame([[i, 0, dict_races[i]]], columns=['race', 'count', 'value']))
 
     race_stat = race_stat.append(pd.DataFrame([[0, notclassified, 'notclassified']], columns=['race', 'count', 'value']))
-    # race_stat = race_stat.set_index('value')
-    # print(race_stat)
-    # race_stat.plot.pie(autopct="%.1f%%", y='count')
-    # plt.legend(loc="lower center", bbox_to_anchor=(0.5, -0.15), ncol=2)
-    # plt.show()
 
     dict_fields_of_study = {1: 'Law', 2:'Math', 3:'Social Science, Psychologist', 4: 'Medical Science, Pharmaceuticals, and Bio Tech',
                             5: 'Engineering', 6: 'English / Creative Writing / Journalism', 7: 'History / Religion / Philosophy',
@@ -162,11 +153,19 @@ if __name__ == '__main__':
 
     df = df.set_index(['iid', 'pid'])
 
+    df.hist(bins=30, figsize=(12, 12))
+    pl.suptitle("Histogram for each numeric input variable")
+    plt.show()
+
     # remove rows with nan race value
     df = df[df['race'].notna()]
 
     # outliers detection
     df = detect_outliers(df, ['int_corr', 'attr1_1', 'sinc1_1', 'intel1_1', 'fun1_1', 'amb1_1', 'shar1_1'])
+
+    df.hist(bins=30, figsize=(12, 12))
+    pl.suptitle("Histogram for each numeric input variable (after outliers detection)")
+    plt.show()
 
     # replace missing values with mean
     df = replace_missing_values(df)
@@ -192,6 +191,28 @@ if __name__ == '__main__':
 
     plt.show()
 
+    # group attributes by gender
+    colors = {0: 'red', 1: 'black'}
+    gender = {0: 'female', 1: 'male'}
+    fig, ax = plt.subplots()
+    grouped = df.groupby('gender')
+    for key, group in grouped:
+        if key % 1 == 0:
+            group.plot(ax=ax, kind='scatter', x='attr1_1', y='age', label=gender[key], color=colors[key], xlim=[0, 60],
+                       ylim=[20, 38])
+    plt.show()
+    # dla kobiet mniejsze znaczenie ma atrakcyjność
+
+    colors = {0: 'red', 1: 'black'}
+    fig, ax = plt.subplots()
+    grouped = df.groupby('gender')
+    for key, group in grouped:
+        if key % 1 == 0:
+            group.plot(ax=ax, kind='scatter', x='amb1_1', y='age', label=gender[key], color=colors[key], xlim=[-0.1, 20],
+                       ylim=[20, 38])
+    plt.show()
+    # dla mężczyzn mniejsze znaczenie mają ambicje
+
 
     # clustering
     cols1, cols2 = loadtxt("args.txt", dtype=str, comments="#", delimiter=",", unpack=False)
@@ -206,15 +227,11 @@ if __name__ == '__main__':
 
     # Principal Component Analysis
     pca_df = run_pca(2, df_to_clustering)
-    pca_df['labels'] = kmeans_model.labels_
-    plt.title('kmeans')
-    sns.scatterplot(x=pca_df.PC0, y=pca_df.PC1, hue=pca_df.labels, palette="Set2")
+    plt.title('K-means')
+    sns.scatterplot(x=pca_df.PC0, y=pca_df.PC1, hue=kmeans_model.labels_, palette="Set2")
     plt.show()
-
-    norm_pca_df = pca_df.copy()
-    norm_pca_df['labels'] = norm_kmeans_model.labels_
-    plt.title('cosine kmeans')
-    sns.scatterplot(x=norm_pca_df.PC0, y=norm_pca_df.PC1, hue=norm_pca_df.labels, palette="Set2")
+    plt.title('Cosine K-means')
+    sns.scatterplot(x=pca_df.PC0, y=pca_df.PC1, hue=norm_kmeans_model.labels_, palette="Set2")
     plt.show()
 
     draw_cluster_barplot(df_to_clustering, norm_kmeans_model)
@@ -237,69 +254,96 @@ if __name__ == '__main__':
     sns.scatterplot(x=df_to_clustering.attr1_1, y=df_to_clustering.sinc1_1, hue=df_to_clustering.labels, palette="Set2")
     plt.show()
 
-    # Make the plot
-    # set all variables between 0 and 1
+    df['gender'].hist(by=df_to_clustering['labels'])
+    pl.suptitle('Gender')
+    plt.show()
+
+    size = df_to_clustering.groupby('labels').size()
+    print(size)
+
+    # Set all variables between 0 and 1
     scaler = preprocessing.MinMaxScaler()
     df_scaled = pd.DataFrame(scaler.fit_transform(df_to_clustering), columns=df_to_clustering.columns)
+    # Make the plot
     plt.figure(figsize=(15, 10))
     parallel_coordinates(df_scaled, class_column='labels', colormap=plt.get_cmap("Set1"))
     plt.xlabel("Features of data set")
     plt.ylabel("Importance")
     plt.show()
 
+    cols2 = [x for x in cols2 if x]
+    print(cols2)
+    df_to_clustering = df.loc[:, cols2]
+
+    kmeans_model, norm_kmeans_model = run_clustering(df_to_clustering)
+
+    # Principal Component Analysis
+    norm_pca_df = run_pca(2, df_to_clustering)
+    plt.title('Cosine K-means')
+    sns.scatterplot(x=norm_pca_df.PC0, y=norm_pca_df.PC1, hue=norm_kmeans_model.labels_, palette="Set2")
+    plt.show()
+
+    draw_cluster_barplot(df_to_clustering, norm_kmeans_model)
+
+    df_to_clustering['labels'] = norm_kmeans_model.labels_
+
+    # wiek, wiek partnera
+    sns.scatterplot(x=df_to_clustering.age, y=df_to_clustering.age_o, hue=df_to_clustering.labels,palette="Set2")
+    plt.show()
+
+    df['gender'].hist(by=df_to_clustering['labels'])
+    pl.suptitle('Gender')
+    plt.show()
+
+    size = df_to_clustering.groupby('labels').size()
+    print(size)
+
+
     # Classification
-    df_to_classification = df.loc[:, cols1]
-    X_train, X_test, y_train, y_test = train_test_split(df_to_classification, df.gender, test_size=0.3)  # 70% training and 30% test
+    feature_cols = ['attr1_1', 'sinc1_1', 'intel1_1', 'fun1_1', 'amb1_1', 'shar1_1']
+    X = df.loc[:, feature_cols]
+    y = df.gender
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+    from sklearn.preprocessing import MinMaxScaler
 
-    # Create KNN Classifier
-    knn = KNeighborsClassifier(n_neighbors=3)
+    scaler = MinMaxScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.transform(X_test)
 
-    # Train the model using the training sets
+    from sklearn.linear_model import LogisticRegression
+
+    logreg = LogisticRegression()
+    logreg.fit(X_train, y_train)
+    print('Accuracy of Logistic regression classifier on training set: {:.2f}'
+          .format(logreg.score(X_train, y_train)))
+    print('Accuracy of Logistic regression classifier on test set: {:.2f}'
+          .format(logreg.score(X_test, y_test)))
+
+    from sklearn.tree import DecisionTreeClassifier
+
+    clf = DecisionTreeClassifier().fit(X_train, y_train)
+    print('Accuracy of Decision Tree classifier on training set: {:.2f}'
+          .format(clf.score(X_train, y_train)))
+    print('Accuracy of Decision Tree classifier on test set: {:.2f}'
+          .format(clf.score(X_test, y_test)))
+
+    from sklearn.neighbors import KNeighborsClassifier
+
+    knn = KNeighborsClassifier()
     knn.fit(X_train, y_train)
+    print('Accuracy of K-NN classifier on training set: {:.2f}'
+          .format(knn.score(X_train, y_train)))
+    print('Accuracy of K-NN classifier on test set: {:.2f}'
+          .format(knn.score(X_test, y_test)))
 
-    # Predict the response for test dataset
     y_pred = knn.predict(X_test)
-
-    # Model Accuracy, how often is the classifier correct?
-    print("Accuracy: ", metrics.accuracy_score(y_test, y_pred))
     cf_matrix = confusion_matrix(y_test, y_pred)
     sns.heatmap(cf_matrix, annot=True, cmap='Blues', fmt='g')
+    plt.show()
     print(classification_report(y_test, y_pred))
-
-    error = []
-
-    # Calculating error for K values between 1 and 40
-    for i in range(1, 40):
-        knn = KNeighborsClassifier(n_neighbors=i)
-        knn.fit(X_train, y_train)
-        pred_i = knn.predict(X_test)
-        error.append(np.mean(pred_i != y_test))
-    plt.figure(figsize=(12, 6))
-    plt.plot(range(1, 40), error, color='red', linestyle='dashed', marker='o', markerfacecolor='blue', markersize=10)
-    plt.title('Error Rate K Value')
-    plt.xlabel('K Value')
-    plt.ylabel('Mean Error')
 
     # female_df = df[df['gender'] == 0].copy()
     # male_df = df[df['gender'] == 1].copy()
     # merged_df = merge_person_partner_data(female_df, male_df)
     #
     # print(merged_df)
-
-    colors = {0: 'red', 1: 'black'}
-    fig, ax = plt.subplots()
-    grouped = df.groupby('gender')
-    for key, group in grouped:
-        if key % 1 == 0:
-            group.plot(ax=ax, kind='scatter', x='attr1_1', y='age', label=key, color=colors[key], xlim=[0,60], ylim=[20,38])
-    plt.show()
-    # dla kobiet mniejsze znaczenie ma atrakcyjność
-
-    colors = {0: 'red', 1: 'black'}
-    fig, ax = plt.subplots()
-    grouped = df.groupby('gender')
-    for key, group in grouped:
-        if key % 1 == 0:
-            group.plot(ax=ax, kind='scatter', x='amb1_1', y='age', label=key, color=colors[key], xlim=[-0.1,20], ylim=[20,38])
-    plt.show()
-    # dla mężczyzn mniejsze znaczenie mają ambicje
